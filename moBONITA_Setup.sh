42bee483260c28f6af17904be3a387b83c6d2841
Palshikar <mpalshikar@urmc-sh.rochester.edu>
2022-05-22 11:51:04 -0400

    Edited setup file

--- moBONITA_Setup.sh
+++ moBONITA_Setup.sh
@@ -5,6 +5,6 @@
 #SBATCH -t 1:00:00
 
 module load anaconda3/2020.07
-source activate scBonita
+source activate BONITA
 
-python3 moBonita_kegg_parser.py -org hsa -sep , --data YOUR_DATAFILE
+python3 moBonita_kegg_parser.py --org hsa --sep , --data YOUR_DATAFILE
