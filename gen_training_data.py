be0c22ae72decf7c1755d50dfc0026a314db4967
Palshikar <mpalshikar@urmc-sh.rochester.edu>
2022-07-14 19:13:00 -0400

    Update gen_training_data.py

--- gen_training_data.py
+++ gen_training_data.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from random import random
 import networkx as nx
+from tensorflow.keras import models, layers, utils, backend as K
+import matplotlib.pyplot as plt
 
 def readData(datafiles = {}):
     """
@@ -92,12 +94,60 @@ if __name__ == "__main__":
     upstream = testNet.in_edges(testNode)
     upstream = [i[0] for i in upstream]
     # subset input data
-    testInput = inputdata.iloc[inputdata.index.get_level_values('Entity').isin(upstream)]
+    testInput = inputdata.iloc[inputdata.index.get_level_values('Entity').isin(upstream)].T
     print(testInput.shape)
     # subset target data
-    testTarget = targetdata.iloc[targetdata.index.get_level_values('Entity').isin([testNode])]
+    testTarget = targetdata.iloc[targetdata.index.get_level_values('Entity').isin([testNode])].T
     print(testTarget.shape)
-    
 
+    model = models.Sequential(name="Perceptron", layers=[
+        layers.Dense(             #a fully connected layer
+            name="dense",
+            input_dim=3,        #with 3 features as the input
+            units=1,            #and 1 node because we want 1 output
+            activation='linear' #f(x)=x
+        )
+    ])
+    model.summary()
+
+    # define metrics
+    def R2(y, y_hat):
+        ss_res =  K.sum(K.square(y - y_hat)) 
+        ss_tot = K.sum(K.square(y - K.mean(y))) 
+        return ( 1 - ss_res/(ss_tot + K.epsilon()) )
+
+    # compile the neural network
+    model.compile(optimizer='adam', loss='mean_absolute_error', 
+                metrics=[R2])
+
+    X = testInput
+    y = testTarget
+    training = model.fit(x=X, y=y, batch_size=2, epochs=10, shuffle=True, verbose=0, validation_split=0.3)
+
+    # plot
+    metrics = [k for k in training.history.keys() if ("loss" not in k) and ("val" not in k)]    
+    fig, ax = plt.subplots(nrows=1, ncols=2, sharey=True, figsize=(15,3))
+        
+    ## training    
+    ax[0].set(title="Training")    
+    ax11 = ax[0].twinx()    
+    ax[0].plot(training.history['loss'], color='black')
+    ax[0].set_xlabel('Epochs')    
+    ax[0].set_ylabel('Loss', color='black')    
+    for metric in metrics:        
+        ax11.plot(training.history[metric], label=metric)
+    ax11.set_ylabel("Score", color='steelblue')    
+    ax11.legend()
+            
+    ## validation    
+    ax[1].set(title="Validation")    
+    ax22 = ax[1].twinx()    
+    ax[1].plot(training.history['val_loss'], color='black')    
+    ax[1].set_xlabel('Epochs')    
+    ax[1].set_ylabel('Loss', color='black')
+    for metric in metrics:          
+        ax22.plot(training.history['val_'+metric], label=metric)
+    ax22.set_ylabel("Score", color="steelblue")    
+    plt.savefig("temp.png")
 
 
