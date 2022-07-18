b3619b46e5396fe404a092223cea5ce4649173b0
mgp13 <32854513+mgp13@users.noreply.github.com>
2022-07-18 10:25:08 -0400

    update

--- gen_training_data.py
+++ gen_training_data.py
@@ -6,10 +6,11 @@ import networkx as nx
 from sklearn.metrics import explained_variance_score
 from tensorflow.keras import models, layers, utils, backend as K
 import tensorflow.keras
+import tensorflow as tf
 import matplotlib.pyplot as plt
 import matplotlib.cm as cmap
 import shap
-
+import random
 
 def readData(datafiles={}):
     """
@@ -139,19 +140,20 @@ def experimentPartOneWrapper():
     #print(targetdata)
     return concatDF, splitDF, inputdata, targetdata
 
-'''
-Use shap to build an explainer.
-:parameter
-    :param model: model instance (after fitting)
-    :param X_names: list
-    :param X_instance: array of size n x 1 (n,)
-    :param X_train: array - if None the model is simple machine learning, if not None then it's a deep learning model
-    :param task: string - "classification", "regression"
-    :param top: num - top features to display
-:return
-    dtf with explanations
-'''
+
 def explainer_shap(model, X_names, X_instance, X_train=None, task="classification", top=10):
+    '''
+    Use shap to build an explainer.
+    :parameter
+        :param model: model instance (after fitting)
+        :param X_names: list
+        :param X_instance: array of size n x 1 (n,)
+        :param X_train: array - if None the model is simple machine learning, if not None then it's a deep learning model
+        :param task: string - "classification", "regression"
+        :param top: num - top features to display
+    :return
+        dtf with explanations
+    '''
     ## create explainer
     ### machine learning
     if X_train is None:
@@ -172,13 +174,13 @@ def explainer_shap(model, X_names, X_instance, X_train=None, task="classificatio
         shap.waterfall_plot(explainer.expected_value[0], shap_values, 
                             features=X_instance, feature_names=X_names, max_display=top)
 
-# define metrics
 def R2(y, y_hat):
     ss_res = K.sum(K.square(y - y_hat))
     ss_tot = K.sum(K.square(y - K.mean(y)))
     return 1 - ss_res / (ss_tot + K.epsilon())
 
-if __name__ == "__main__":
+def experimentPartTwoWrapper():
+    """Simple perceptron"""
     # prepare data
     concatDF, splitDF, inputdata, targetdata = experimentPartOneWrapper()
     # prepare network
@@ -346,3 +348,159 @@ if __name__ == "__main__":
     for tn in answers.keys():
         if sum(answers[tn][2]) > 0:
             print(answers[tn])
+
+def xorTutorial():
+    import tensorflow.compat.v1 as tf
+    tf.disable_v2_behavior()
+    # input X vector
+    X = [[0, 0], [0, 1], [1, 0], [1, 1]]
+    # output Y vector
+    Y = [[0], [1], [1], [0]]
+    
+    # Placeholders for input and output
+    x = tf.placeholder(tf.float32, shape=[4,2])
+    y = tf.placeholder(tf.float32, shape=[4,1])
+    
+    # W matrix
+    W1 = tf.Variable([[1.0, 0.0], [1.0, 0.0]], shape=[2,2])
+    W2 = tf.Variable([[0.0], [1.0]], shape=[2,1])
+    
+    # Biases
+    B1 = tf.Variable([0.0, 0.0], shape=[2])
+    B2 = tf.Variable([0.0], shape=1)
+    
+    # Hidden layer and outout layer
+    output =tf.sigmoid(tf.matmul(tf.sigmoid(tf.matmul(x, W1) + B1), W2) + B2)
+    
+    # error estimation
+    e = tf.reduce_mean(tf.squared_difference(y, output))
+    train = tf.train.GradientDescentOptimizer(0.1).minimize(e)
+    
+    init = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init)
+    
+    for i in range (100001):
+        error = sess.run(train, feed_dict={x: X, y: Y})
+        if i % 10000 == 0:
+            print('\nEpoch: ' + str(i))
+            print('\nError: ' + str(sess.run(e, feed_dict={x: X, y: Y})))
+            for el in sess.run(output, feed_dict={x: X, y: Y}):
+                print('    ',el)
+    sess.close()
+    
+    print ("Complete")
+
+
+def xorDeepNN():
+    import numpy as np
+    # input X vector
+    X1 = pd.DataFrame(np.repeat(np.array([[0, 0], [0, 1], [1, 0], [1, 1]]), 100, axis = 0))
+    print(X1)
+    # output Y vector
+    Y1 = pd.DataFrame(np.repeat(np.array([[0], [1], [1], [0]]), 100, axis =  0))
+    print(Y1)
+    model = models.Sequential(name="DeepNN", layers=[
+    ### hidden layer 1
+    layers.Dense(name="h1", input_dim=2,
+                units=int(2), 
+                activation='relu'),
+    layers.Dropout(name="drop1", rate=0.2),
+    ### hidden layer 2
+    layers.Dense(name="h2", units=int(1), 
+                activation='linear'),
+    layers.Dropout(name="drop2", rate=0.2),
+    
+    ### layer output
+    layers.Dense(name="output", units=1, activation='sigmoid')
+    ])
+    model.summary()
+    
+    # compile the neural network
+    model.compile(
+        optimizer="adam",
+        loss="mean_absolute_error",
+        metrics=[
+            #tensorflow.keras.metrics.AUC(),
+            #tensorflow.keras.metrics.FalsePositives(),
+            #tensorflow.keras.metrics.FalseNegatives(),
+            tensorflow.keras.metrics.BinaryAccuracy(),
+            R2,
+            #tensorflow.keras.metrics.Recall(),
+        ],
+    )
+    n_samples = len(X1)
+    trainingSamples = random.sample(range(0, n_samples), floor(3*n_samples/4))
+    testSamples = list(set(range(0,n_samples)).difference(set(trainingSamples)))
+    X = X1.iloc[trainingSamples]
+    y = Y1.iloc[trainingSamples]
+    training = model.fit(
+        x=X, y=y, batch_size=10, epochs=64, shuffle=False, verbose=0, validation_split=0.3
+    )
+
+    # plot
+    metrics = [
+        k for k in training.history.keys() if ("loss" not in k) and ("val" not in k)
+    ]
+    fig, ax = plt.subplots(nrows=1, ncols=2, sharey=True, figsize=(15, 3))
+
+    ## training
+    ax[0].set(title="Training")
+    ax11 = ax[0].twinx()
+    ax[0].plot(training.history["loss"], color="black")
+    ax[0].set_xlabel("Epochs")
+    ax[0].set_ylabel("Loss", color="black")
+    colors = ["blue", "red", "orange"]
+    i = 0
+    for metric in metrics:
+        color = colors[i]
+        i = i + 1
+        ax11.plot(training.history[metric], label=metric, color = color)
+    ax11.set_ylabel("Score", color="red")
+    ax11.legend()
+
+    ## validation
+    i = 0
+    ax[1].set(title="Validation")
+    ax22 = ax[1].twinx()
+    ax[1].plot(training.history["val_loss"], color="black")
+    ax[1].set_xlabel("Epochs")
+    ax[1].set_ylabel("Loss", color="black")
+    for metric in metrics:
+        ax22.plot(training.history["val_" + metric], label=metric, color=colors[i])
+        i = i + 1
+    ax22.set_ylabel("Score", color="red")
+    ax11.legend()
+    plt.savefig("temp.png")
+    plt.close()
+
+    ## explainer_shap(model, upstream, X, X_train=X, task="regression", top=10)
+    from numpy import argmax
+    test_predictions = argmax(model.predict(X1.iloc[testSamples]),axis=-1).flatten()
+
+    a = plt.axes(aspect='equal')
+    plt.scatter(Y1.iloc[testSamples], test_predictions)
+    plt.xlabel('True Values')
+    plt.ylabel('Predictions')
+    lims = [0, 1]
+    plt.xlim(lims)
+    plt.ylim(lims)
+    plt.savefig('temp2.png')
+
+    print(test_predictions)
+    #print(testTarget.iloc[testSamples])
+    #print(len(testTarget.iloc[testSamples]))
+    answer = Y1.iloc[testSamples]
+    answer = answer.iloc[:,0]
+    answer = answer.to_list()
+    print(answer)
+    print(len(answer))
+    print(sum([1 if i == j else 0 for i, j in zip(test_predictions,answer)]))
+
+
+if __name__ == "__main__":
+    # prepare data
+    concatDF, splitDF, inputdata, targetdata = experimentPartOneWrapper()
+    # prepare network
+    testNet = nx.read_graphml("large_hif1Agraph.graphml")
+    
