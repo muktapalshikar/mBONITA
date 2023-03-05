f76081e6649bddd0d409dc9276a3ad62d17fbda7
Mukta G. Palshikar <32854513+mgp13@users.noreply.github.com>
2023-03-05 11:55:38 -0500

    documentation

--- mBONITA module/run_pathway_analysis.sh
+++ mBONITA module/run_pathway_analysis.sh
@@ -9,9 +9,8 @@
 module load anaconda3/2020.11
 activate BONITA
 #Template:
-#python3 pathway_analysis_score_pathways.py Your_omics_data Your_condition_matrix Your_desired_contrasts -sep Separator_used_in_gmt_and_omics_data
+#python3 pathway_analysis_score_pathways_mBonita.py Your_omics_data Your_condition_matrix Your_desired_contrasts -sep Separator_used_in_gmt_and_omics_data
 
 #Example
-#python3 pathway_analysis_score_pathways.py concatenated_datasets.csv concatenated_conditions.csv contrasts.csv -sep ,
 
 python3 pathway_analysis_score_pathways_mBonita.py concatenated_datasets.csv concatenated_conditions.csv contrasts.csv -sep ,
